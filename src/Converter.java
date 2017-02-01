6b46736bcdbc533e569c3287525e8388041098e3
nbakshi15 <nirali.bakshi@yahoo.com>
2017-01-31 21:24:04 -0500

    Sent an email explaining

---
+++ src/Converter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by nini on 1/26/2017.
+ */
+public class Converter {
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello! Welcome to Crystal Color Converter!!");
+        System.out.println("Enter 1 if you'd like to enter a Hexadecimal");
+        System.out.println("Enter 2 if you'd like to enter a HTML name");
+        System.out.println("Enter 3 if you'd like to enter an RGB Triplet");
+        String begin = input.next();
+
+        if (begin.equalsIgnoreCase("1")) {
+
+            Storage j = new Storage();
+
+            System.out.println("Enter a Hexidecimal number");
+
+            String response = input.next();
+
+            j.Converter(response);
+
+        } else if (begin.equalsIgnoreCase("2")) {
+
+            Storage j = new Storage();
+
+            System.out.println("Enter any common HTML name");
+
+            String response = input.next();
+
+            j.Converter(response);
+
+        } else if (begin.equalsIgnoreCase("3")) {
+            Storage j = new Storage();
+
+            System.out.println("Enter an RGB triplet");
+
+            String response = input.next();
+
+            j.Converter(response);
+
+        }
+        else
+            System.out.println("Error has been found");
+
     }
-}
+    }
